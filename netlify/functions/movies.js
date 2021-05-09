aaae074090c2663c5e5093a8293aa097b3ea1a6c
Chelsea Young <chelsea.young@kellogg.northwestern.edu>
2021-05-08 23:12:02 -0400

    hw6 commit

--- netlify/functions/movies.js
+++ netlify/functions/movies.js
@@ -3,6 +3,7 @@ let csv = require('neat-csv')
 
 // allows us to read files from disk
 let fs = require('fs')
+const { stringify } = require('querystring')
 
 // defines a lambda function
 exports.handler = async function(event) {
@@ -25,7 +26,7 @@ exports.handler = async function(event) {
   if (year == undefined || genre == undefined) {
     return {
       statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-      body: `Nope!` // a string of data
+      body: `Error: Please enter year and genre` // a string of data
     }
   }
   else {
@@ -35,13 +36,38 @@ exports.handler = async function(event) {
     }
 
     for (let i=0; i < moviesFromCsv.length; i++) {
+      // store movies in memory  
+      let movies = moviesFromCsv[i]
 
+      // create if loop for all movies with specified year and genre (using user inputted year and genre)
+
+      // genre assumes to include other genres as well 
+
+      if(year == movies.startYear && movies.genres.includes(genre) && movies.runtimeMinutes != `\\N` && movies.genres != `\\N`) { 
+
+      // add number of results to the returnValue 
+      
+      returnValue.numResults = returnValue.numResults + 1
+
+      // create object to hold movies array detail
+
+      let postObject = {
+        title: movies.primaryTitle,
+        year: movies.startYear,
+        genre: movies.genres
+      }
+      
+      // add results from if loop of to the movies array including primary title, year movie release, movie genres 
+      
+      returnValue.movies.push(postObject)
+
+      }
     }
 
     // a lambda function returns a status code and a string of data
     return {
       statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-      body: `Hello from the back-end!` // a string of data
+      body: JSON.stringify(returnValue) // a string of data
     }
   }
 }
